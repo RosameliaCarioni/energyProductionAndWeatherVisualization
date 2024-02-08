af6782e80d5ac506256af2e732b4052facb1469c
MischaRauch <mischa1297@gmx.de>
2024-02-08 19:07:26 +0100

    client weatherlayers without error

--- src/components/MapComponent.client.js
+++ src/components/MapComponent.client.js
@@ -3,6 +3,9 @@
 import React, { useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl"; // Import Mapbox GL
 import "mapbox-gl/dist/mapbox-gl.css"; // Import stylesheet
+//import "@weatherlayers/gl/dist/weatherlayers-gl.css"; // Import WeatherLayers stylesheet
+//import WeatherLayers from "@weatherlayers/gl"; // Import WeatherLayers GL JS
+import * as WeatherLayersClient from 'weatherlayers-gl/client';
 
 const MapComponent = () => {
   const mapContainerRef = useRef(null);
@@ -15,6 +18,18 @@ const MapComponent = () => {
       style: "mapbox://styles/mapbox/streets-v11",
       center: [0, 0],
       zoom: 1,
+    });
+
+    // Initialize WeatherLayers after the map loads
+    map.on('load', () => {
+      const client = new WeatherLayersClient.Client({
+        accessToken: process.env.NEXT_PUBLIC_WEATHERLAYERS_ACCESS_TOKEN, // Replace with your WeatherLayers access token
+        datetimeInterpolate: true,
+        //map: map, // Pass the Mapbox map instance
+      });
+      
+      // Add a specific weather layer, for example, 'temperature'
+      //client.addLayer("temperature");
     });
 
     return () => map.remove();
