cd27e219c4db07ff8a90c3bdef29e48b998ceee2
MischaRauch <mischa1297@gmx.de>
2024-02-13 18:18:29 +0100

    made a merge mistake - now up to date with main

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,7 @@
-import dynamic from "next/dynamic";
-import MapComponent from "@/components/MapComponent.client";
-import React from 'react';
-
-// const MapComponentWithNoSSR = dynamic(
-//   () => import("../components/MapComponent.client"),
-//   {
-//     ssr: false,
-//   }
-// );
-
-// Dynamically import the MapComponent with SSR disabled
-const MapComponentWithNoSSR = dynamic(
-  () => import('../components/MapComponent.client'), // Update the path to where your MapComponent is located
-  { ssr: false }
-);
-
-const Page = () => {
-  return (
-    <div>
-      <MapComponentWithNoSSR />
-    </div>
-  );
-};
-
-export default Page;
+export default function WelcomePage(){
+  return(
+      <div>
+          <h1> This is the home page with how to use, etc.  </h1>
+      </div>
+  )
+}
