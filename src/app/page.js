91a5b859061142e00d17454778f135670580754d
Danina Jansson <danina@kth.se>
2024-02-15 11:03:08 +0100

    welcome page updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,19 @@
+import '../output.css';
+
 export default function WelcomePage(){
   return(
-      <div>
-          <h1> This is the home page with how to use, etc.  </h1>
+    <div>
+      <div className="page-container">
+        <div className="content-container">
+          <div className="section">
+            <h1>Welcome page</h1>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+          </div>
+        </div>
+        <img width="100%" src="lineImg.svg" alt="line background"/>
+        <p>IVIS 2024</p>
       </div>
+    </div>
+    
   )
 }
