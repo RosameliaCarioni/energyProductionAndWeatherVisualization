2cbf73ea83671bfbd0718624b41987ae37ac63f8
Samuel Söderberg <sasoder@kth.se>
2024-02-17 16:55:35 +0100

    timeslider on mapcomponent + annotation on graph

--- src/components/GraphComponent.js
+++ src/components/GraphComponent.js
@@ -13,7 +13,8 @@ import {
   PointElement,
   LineElement,
 } from "chart.js";
-import { Bar, Line } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
+import annotationPlugin from 'chartjs-plugin-annotation';
 
 ChartJS.register(
   CategoryScale,
@@ -23,53 +24,49 @@ ChartJS.register(
   LineElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
+  annotationPlugin
 );
 
-function GraphComponent(props) {
-  console.log(props.graphValues);
+function GraphComponent({graphValues, chartTitle, selectedTime, selectedDate}) {
+
+  const hasData = graphValues && graphValues.length > 0;
+  const selectedLabel = `PH${selectedTime}`;
   const chartData = {
     //x-axis:
-    labels: [
-      "00.00",
-      "01.00",
-      "02.00",
-      "03.00",
-      "04.00",
-      "05.00",
-      "06.00",
-      "07.00",
-      "08.00",
-      "09.00",
-      "10.00",
-      "11.00",
-      "12.00",
-      "13.00",
-      "14.00",
-      "15.00",
-      "16.00",
-      "17.00",
-      "18.00",
-      "19.00",
-      "20.00",
-      "21.00",
-      "22.00",
-      "23.00",
-      "24.00",
-    ],
+    labels: ["PH1", "PH2", "PH3", "PH4", "PH5", "PH6", "PH7", "PH8", "PH9", "PH10", "PH11", "PH12", "PH13", "PH14", "PH15", "PH16", "PH17", "PH18", "PH19", "PH20", "PH21", "PH22", "PH23", "PH24"],
 
     //y-values:
     datasets: [
       {
-        label: props.chartTitle,
-        data: props.graphValues,
+        label: chartTitle || 'No Data',
+        data: hasData ? graphValues : Array(24).fill(0),
         borderColor: "rgb(135, 211, 184)",
       },
     ],
   };
+  const options = {
+    maintainAspectRatio: false,
+    plugins: {
+      annotation: {
+        annotations: {
+          line1: {
+            type: 'line',
+            yMin: 0,
+            yMax: chartData.datasets[0].data.reduce((a, b) => Math.max(a, b), 0),
+            xMin: chartData.labels.indexOf(selectedLabel),
+            xMax: chartData.labels.indexOf(selectedLabel),
+            borderColor: 'rgba(255, 99, 132, 0.5)',
+            borderWidth: 2,
+          },
+        },
+      },
+    },
+  };
+
   return (
     <div style={{ width: '400px', height: '200px' }}>
-      <Line data={chartData} options={{ maintainAspectRatio: false }}/>
+      <Line data={chartData} options={options}/>
     </div>
   );
 }
--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -9,7 +9,7 @@ import { ClipExtension } from '@deck.gl/extensions';
 import MapGL, { Popup, Marker } from 'react-map-gl';
 import {getFarmsMeta} from "@/utils/getFarmsMetaData"
 
-function MapComponent({onSelectPlant, selectedPlant}) {
+function MapComponent({onSelectPlant, selectedPlant, children}) {
   const [viewState, setViewState] = useState({
     latitude: 60.4720,
     longitude: 8.4689,
@@ -54,12 +54,6 @@ function MapComponent({onSelectPlant, selectedPlant}) {
 
   const onMapLoad = useCallback(async (event) => {
     const map = event.target;
-      // Log the map instance itself
-  console.log("Map instance:", map);
-  // Log specific properties of the map's state
-  console.log("Map center:", map.getCenter());
-  console.log("Map zoom:", map.getZoom());
-  console.log("Map bounds:", map.getBounds().toArray());
 
     const weatherLayersToken = process.env.NEXT_PUBLIC_WEATHERLAYERS_ACCESS_TOKEN;
 
@@ -131,59 +125,65 @@ function MapComponent({onSelectPlant, selectedPlant}) {
     
 
   return (
-    <MapGL
-    {...viewState}
-      onMove={evt => setViewState(evt.viewState)}
-      style={{width: '100%', height: '750px'}}
-      mapStyle="mapbox://styles/iv24/clsq58r47006b01pk05dpavbj"
-      projection={"mercator"}
-      mapboxAccessToken={mapboxToken}
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-      onLoad={onMapLoad}
-    >
-      {plantsArray.map(plant => (
-        <Marker
-          key={plant.id}
-          latitude={plant.latitude}
-          longitude={plant.longitude}
-          anchor="bottom"
-        >
-          <div
-            onMouseEnter={() =>{
-              setHoverInfo(plant)
-              console.log(plant)
-            }}
-            onMouseLeave={() => setHoverInfo(undefined)}
-            onClick={() => handleMarkerClick(plant)}
-            style={{ cursor: 'pointer' }}
+    <div className="relative w-full h-full">
+      <MapGL
+      {...viewState}
+        onMove={evt => setViewState(evt.viewState)}
+        style={{width: '100%', height: '750px'}}
+        mapStyle="mapbox://styles/iv24/clsq58r47006b01pk05dpavbj"
+        projection={"mercator"}
+        mapboxAccessToken={mapboxToken}
+        onViewportChange={nextViewport => setViewport(nextViewport)}
+        onLoad={onMapLoad}
+      >
+        {plantsArray.map(plant => (
+          <Marker
+            key={plant.id}
+            latitude={plant.latitude}
+            longitude={plant.longitude}
+            anchor="bottom"
           >
-            <img src ={(selectedPlant && selectedPlant.id == plant.id) || (hoverInfo && hoverInfo.id == plant.id) ? "/assets/pin_selected.svg" : "/assets/pin.svg"} alt="Marker" style={{ width: '30px', height: '30px'}}/>
-          </div>
-        </Marker>
-      ))}
+            <div
+              onMouseEnter={() =>{
+                setHoverInfo(plant)
+                console.log(plant)
+              }}
+              onMouseLeave={() => setHoverInfo(undefined)}
+              onClick={() => handleMarkerClick(plant)}
+              style={{ cursor: 'pointer' }}
+            >
+              <img src ={(selectedPlant && selectedPlant.id == plant.id) || (hoverInfo && hoverInfo.id == plant.id) ? "/assets/pin_selected.svg" : "/assets/pin.svg"} alt="Marker" style={{ width: '30px', height: '30px'}}/>
+            </div>
+          </Marker>
+        ))}
 
-      {hoverInfo && (
-        <Popup
-          latitude={hoverInfo.latitude}
-          longitude={hoverInfo.longitude}
-          closeButton={false}
-          closeOnClick={false}
-          ref={popupRef}
-          anchor="bottom"
-          className="z-50"
-          offset={30}
-          maxWidth="500px"
-        >
-          <div className="p-4 bg-white shadow-lg rounded-lg max-w-xs text-sm">
-            <h1 className="text-lg font-bold mb-2">{hoverInfo.name}</h1>
-            <p className="mb-1">Capacity: <span className="font-semibold">{hoverInfo.capacity_kw} kw</span></p>
-            <p className="mb-1">Latitude: <span className="font-semibold">{hoverInfo.latitude}</span></p>
-            <p className="mb-1">Longitude: <span className="font-semibold">{hoverInfo.longitude}</span></p>
-            <p>ID: <span className="font-semibold">{hoverInfo.id}</span></p>
-          </div>
-        </Popup>
-      )}
-    </MapGL>
+        {hoverInfo && (
+          <Popup
+            latitude={hoverInfo.latitude}
+            longitude={hoverInfo.longitude}
+            closeButton={false}
+            closeOnClick={false}
+            ref={popupRef}
+            anchor="bottom"
+            className="z-50"
+            offset={30}
+            maxWidth="500px"
+          >
+            <div className="p-4 bg-white shadow-lg rounded-lg max-w-xs text-sm">
+              <h1 className="text-lg font-bold mb-2">{hoverInfo.name}</h1>
+              <p className="mb-1">Capacity: <span className="font-semibold">{hoverInfo.capacity_kw} kw</span></p>
+              <p className="mb-1">Latitude: <span className="font-semibold">{hoverInfo.latitude}</span></p>
+              <p className="mb-1">Longitude: <span className="font-semibold">{hoverInfo.longitude}</span></p>
+              <p>ID: <span className="font-semibold">{hoverInfo.id}</span></p>
+            </div>
+          </Popup>
+        )}
+
+        <div className="absolute inset-x-0 bottom-0 p-4 flex justify-center">
+          {children}
+        </div>
+      </MapGL>
+    </div>
   );
 };
 
