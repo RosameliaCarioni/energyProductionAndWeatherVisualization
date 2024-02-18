a0d2d6df63261c6f80016c0c03e03b607ac4813a
elinlqv <elinlqv@gmail.com>
2024-02-18 18:21:52 +0100

    added styling to the graphs

--- src/components/GraphComponent.js
+++ src/components/GraphComponent.js
@@ -12,9 +12,10 @@ import {
   Legend,
   PointElement,
   LineElement,
+  Colors,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
-import annotationPlugin from 'chartjs-plugin-annotation';
+import annotationPlugin from "chartjs-plugin-annotation";
 
 ChartJS.register(
   CategoryScale,
@@ -25,39 +26,99 @@ ChartJS.register(
   Title,
   Tooltip,
   Legend,
-  annotationPlugin
+  annotationPlugin,
+  Colors
 );
 
-function GraphComponent({graphValues, chartTitle, selectedTime, selectedDate}) {
-
+function GraphComponent({
+  graphValues,
+  chartTitle,
+  selectedTime,
+  selectedDate,
+}) {
   const hasData = graphValues && graphValues.length > 0;
   const selectedLabel = `PH${selectedTime}`;
   const chartData = {
     //x-axis:
-    labels: ["PH1", "PH2", "PH3", "PH4", "PH5", "PH6", "PH7", "PH8", "PH9", "PH10", "PH11", "PH12", "PH13", "PH14", "PH15", "PH16", "PH17", "PH18", "PH19", "PH20", "PH21", "PH22", "PH23", "PH24"],
+    labels: [
+      "PH1",
+      "PH2",
+      "PH3",
+      "PH4",
+      "PH5",
+      "PH6",
+      "PH7",
+      "PH8",
+      "PH9",
+      "PH10",
+      "PH11",
+      "PH12",
+      "PH13",
+      "PH14",
+      "PH15",
+      "PH16",
+      "PH17",
+      "PH18",
+      "PH19",
+      "PH20",
+      "PH21",
+      "PH22",
+      "PH23",
+      "PH24",
+    ],
 
     //y-values:
     datasets: [
       {
-        label: chartTitle || 'No Data',
+        label: chartTitle || "No Data",
         data: hasData ? graphValues : Array(24).fill(0),
         borderColor: "rgb(135, 211, 184)",
+        backgroundColor: "rgb(135, 211, 184, 0.25)",
       },
     ],
   };
   const options = {
     maintainAspectRatio: false,
+
+    scales: {
+      y: {
+        grid: {
+          color: "rgb(98,98,98)",
+        },
+        ticks: {
+          color: "rgb(214,214,214)",
+        },
+      },
+      x: {
+        grid: {
+          color: "rgb(98,98,98)",
+        },
+        ticks: {
+          color: "rgb(214,214,214)",
+        },
+      },
+    },
     plugins: {
+      legend: {
+        labels: {
+          color: "rgb(214,214,214)",
+          fontSize: 18,
+        },
+      },
       annotation: {
         annotations: {
           line1: {
-            type: 'line',
+            type: "line",
             yMin: 0,
-            yMax: chartData.datasets[0].data.reduce((a, b) => Math.max(a, b), 0),
+            yMax: chartData.datasets[0].data.reduce(
+              (a, b) => Math.max(a, b),
+              0
+            ),
             xMin: chartData.labels.indexOf(selectedLabel),
             xMax: chartData.labels.indexOf(selectedLabel),
-            borderColor: 'rgba(255, 99, 132, 0.5)',
-            borderWidth: 2,
+            //colors controlling the timeslider
+            borderColor: "rgba(255, 99, 132)",
+            borderWidth: 3,
           },
         },
       },
@@ -65,8 +126,8 @@ function GraphComponent({graphValues, chartTitle, selectedTime, selectedDate}) {
   };
 
   return (
-    <div style={{ width: '400px', height: '200px' }}>
-      <Line data={chartData} options={options}/>
+    <div className="graph">
+      <Line data={chartData} options={options} />
     </div>
   );
 }
