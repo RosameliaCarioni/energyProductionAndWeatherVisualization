c89ad2c0348bb18df9a1a3a2cb8f26e9f4f6baea
MischaRauch <mischa1297@gmx.de>
2024-02-20 20:41:12 +0100

    another one

--- src/utils/endpoint.js
+++ src/utils/endpoint.js
@@ -1,2 +1,2 @@
 //export const endpoint = 'http://localhost:3000/api/'
-export const endpoint = 'https://weather-visualization-rebase.vercel.app/api/' 
+export const endpoint = 'https://weather-visualization-rebase-l0danzh2u-mischarauchs-projects.vercel.app/api/' 
