a7d6c8fc089040c5def44f120e705d030b9cad08
MischaRauch <mischa1297@gmx.de>
2024-02-20 21:17:20 +0100

    vercel test

--- src/app/(routes)/graphs/page.js
+++ src/app/(routes)/graphs/page.js
@@ -4,11 +4,11 @@ import { getWindSpeed } from "@/utils/getWindSpeed";
 
 export default async function Graphs() {
   //fetching the plant data
-  const energyData = await getProduction(6, 2021, 6, 19);
-  console.log("Energydata:", energyData);
+  //const energyData = await getProduction(6, 2021, 6, 19);
+  //console.log("Energydata:", energyData);
   //fetching the wind data
-  const windData = await getWindSpeed(6, 2021, 6, 19);
-  console.log("Winddata:", windData);
+  //const windData = await getWindSpeed(6, 2021, 6, 19);
+  //console.log("Winddata:", windData);
 
   return (
     <div>
