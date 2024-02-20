15c0f1f47e7fb63e4440c9e9f2f563938ddfec88
Danina Jansson <danina@kth.se>
2024-02-20 08:38:03 +0100

    sidebarcomponent css updated

--- src/components/SideBarComponent.js
+++ src/components/SideBarComponent.js
@@ -3,12 +3,12 @@ import Link from "next/link";
 
 export default function SideBarComponent(){
     return (
-        <div className="sidebar-container">
+        <div className="absolute h-full ">
             
             <Link href = "/">
                 <img
                     id="home"
-                    className="nav-icon"
+                    className="p-3 w-full mb-2"
                     src="/assets/icons/home-w.svg" 
                     alt="Home"/>
             </Link>
@@ -16,7 +16,7 @@ export default function SideBarComponent(){
             <Link href = "/map">
                 <img 
                     id="map"
-                    className="nav-icon"
+                    className="p-3 mb-2"
                     src="/assets/icons/map-w.svg" 
                     alt="Map"/>
             </Link>
@@ -24,7 +24,7 @@ export default function SideBarComponent(){
             <Link href = "/listFarmsInformation">
                 <img 
                     id="list"
-                    className="nav-icon"
+                    className="p-3 mb-2"
                     src="/assets/icons/list-w.svg" 
                     alt="List"/>
             </Link>
@@ -32,7 +32,7 @@ export default function SideBarComponent(){
             <Link href = "/about">
                 <img 
                     id="about"
-                    className="nav-icon"
+                    className="p-3 mb-2"
                     src="/assets/icons/about-w.svg" 
                     alt="About"/>
             </Link>
